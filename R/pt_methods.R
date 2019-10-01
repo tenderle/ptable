493e2ff9ecd9546eeb83cd45c29fd369835cd4ec
Tobias Enderle <tobias.enderle@destatis.de>
2019-10-01 10:10:47 +0200

    Version 0.3.2 (#22)
    * fix class definition (#10)
    
    * minor bugfixing
    
    * Exp2Master (#12)
    
    * Version 0.3.0
    
    * minor bugfixes to version 0.3.0
    
    * Version 0.3.1 (minor bugfixes of version 0.3.0)
    
    * Version 0.3.2
    
    * add two functions that return (simple) ptables and which can easily be used in examples (and cellKey) (#11)
    
    * Updated examples
    
    * Updated vignette
    
    * Updated examples for main functions
    
    * Exp2Master (#13)
    
    * Version 0.3.0
    
    * minor bugfixes to version 0.3.0
    
    * Version 0.3.1 (minor bugfixes of version 0.3.0)
    
    * Version 0.3.2
    
    * add two functions that return (simple) ptables and which can easily be used in examples (and cellKey) (#11)
    
    * Updated examples
    
    * Updated vignette
    
    * Updated examples for main functions
    
    * minor updates (argument icat, vignette)
    
    * minor update
    
    * minor updates due to merge exp-branch
    
    * Exp2Master (#15)
    
    * Version 0.3.0
    
    * minor bugfixes to version 0.3.0
    
    * Version 0.3.1 (minor bugfixes of version 0.3.0)
    
    * Version 0.3.2
    
    * add two functions that return (simple) ptables and which can easily be used in examples (and cellKey) (#11)
    
    * Updated examples
    
    * Updated vignette
    
    * Updated examples for main functions
    
    * minor updates (argument icat, vignette)
    
    * minor update
    
    * revert parametrisation of argument `parity` (#14)
    
    * help file bugfixes due to merge with branch exp
    
    * update readme

--- R/pt_methods.R
+++ R/pt_methods.R
@@ -1,9 +1,23 @@
-#' Method to extract results
-#' @name results
-#' @rdname results-methods
-#' @param x input object
+#' Method to plot results
+#' @name plot
+#' @rdname plot
+#' @param obj an object of class \code{\linkS4class{ptable}}
+#' @param type (character) type of graph: distribution "d" (standard), perturbation panel ("p"), transition matrix "t"
+#' @param file if not \code{NULL}, a path to a file where the graph is saved to as pdf
 #' @param ... additional parameters passed to methods
-#' @exportMethod results
-setGeneric("results", function(x, ...) {
-  standardGeneric("results")
+#' 
+#' @examples
+#' # Simple Example
+#' params <- pt_create_pParams(D=2, V=1.08,js=1, mono=FALSE, label="Example")
+#' ptable_destatis <- pt_create_pTable(params=params)
+#' plot(ptable_destatis, type="d")
+#' 
+#' \dontrun{
+#' ## Export result
+#' plot(ptable_destatis, type="d", file="graph.pdf")
+#' }
+#' 
+#' @exportMethod plot
+setGeneric("plot", function(obj, type="d", file=NULL, ...) {
+  fifi_plot(obj, type=type, file=file,...)
 })
