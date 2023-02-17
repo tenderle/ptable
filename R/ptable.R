bcb5b7de3d427122df121e1603b141ba1dcfdf8e
tenderle <tobias.enderle@destatis.de>
2023-02-17 10:45:10 +0100

    update dashboard

--- R/ptable.R
+++ R/ptable.R
@@ -1,9 +1,28 @@
-#' ptable
-#'
-#' Perturbation Table Visualizer
-#'
+#' @title Perturbation Table Dashboard for Frequency Count Tables
+#' @description In the `ptable`-package there is a shiny app for first 
+#' time users and visual-style learners. [ptable()] makes it easy to experiment 
+#' with different parameter
+#' settings while getting direct feedback by means of graphical plots and 
+#' summaries. The different result output tabs are:
+#' 
+#' * `Perturbation Table` shows the output used for applying CKM methods. 
+#' * `Constraints Check` can be used to check the main constraints (e.g., zero mean,
+#' fixed variance)
+#' * `Input Code` could be used for replication of the results (i.e. copy&paste the
+#' code for your R script).
+#' * `Input Object` shows the input object derived from the parameters a user set. 
+#' * `Legend` gives an overview of used parameters.
+#' 
+#' Users can also visually learn how input parameters effect 
+#' the perturbation table:
+#' 
+#' * `Transition Matrix`
+#' * `Distribution` Plot
+#' * `Perturbation Panel` Plot
 #' @md
 #'
+#' @seealso 
+#' [ptable_pkg()] to get more help or [pt_vignette()] for an introduction
 #'
 #' @author Tobias Enderle, \email{tobias.enderle@@destatis.de}
 #' @keywords perturbation table visualisation dashboard flexdashboard shiny
